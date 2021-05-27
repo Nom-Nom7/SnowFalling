b435cfcb50fd08bfafb0ca51fe0f65e56b53cbab
Nom-Nom7 <76200202+Nom-Nom7@users.noreply.github.com>
2021-05-27 11:36:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var backgroundImg;
+var snowimg;
+
+var snowflakes = [];
+
+function preload() {
+  backgroundImg = loadImage("snow3.jpg");
+  snowimg = loadImage("snow4.webp");
+}
+
+function setup() {
+  createCanvas(800,400);
+}
+
+function draw() {
+  background(backgroundImg);  
+
+  if(frameCount % 90 === 0){
+    snowflakes.push(new Snow(random(width/2-10,width/2+10),10,10));
+  }
+
+  for (var e = 0; e < snowflakes.length; e++) {
+    snowflakes[e].display();
+  }
+
+  drawSprites();
+}
